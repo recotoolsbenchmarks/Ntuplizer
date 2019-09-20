9034e93b952085a5f0ee6bb19d6d7c3193f1208e
sandhya jain <sandhya.jain@cern.ch>
2019-09-20 11:46:57 +0200

    Updating photons reliso

--- TreeMaker/Ntuplzr/plugins/Validator.cc
+++ TreeMaker/Ntuplzr/plugins/Validator.cc
@@ -470,7 +470,7 @@ Validator::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
       gamma_phi[gamma_size]       = photns->at(ip).phi();
       gamma_mass[gamma_size]      = photns->at(ip).mass();
       gamma_idvar[gamma_size]     = mvaValue; // MVA
-      gamma_reliso[gamma_size]    = 1.;
+      gamma_reliso[gamma_size]    = 0.;
       
       
       if(isLoose)
